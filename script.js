66593c54e14ad99cdfe3adb6b7b3397a7f266595
dominion-30 <abiolaayobamidele@gmail.com>
2023-01-17 15:35:58 +0100

    Add file

---
+++ script.js
@@ -0,0 +1,10 @@
+// Program to get a number from the user that is between 1 and 10
+
+let user_num = prompt("Enter number from 1 - 10", "Enter number");
+
+if (user_num >= 1 && user_num <=10){
+    alert("Bravo, you are right");
+}
+else{
+    alert("How could you be so wrong");
+}
