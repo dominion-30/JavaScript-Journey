a3f5ea70ceff0c8a83b60e59e51bbd392ca3427f
dominion-30 <abiolaayobamidele@gmail.com>
2023-02-14 16:26:23 +0100

    add changes

--- script.js
+++ script.js
@@ -600,16 +600,33 @@
 //         alert("An unknown number by this system");
 // }
 
-let browser = prompt("Enter the name of your favourite browser", "Favourite browser");
+// let browser = prompt("Enter the name of your favourite browser", "Favourite browser");
 
-if (browser == "Edge"){
-    alert("You've got the Edge!");
-}
+// if (browser == "Edge"){
+//     alert("You've got the Edge!");
+// }
 
-else if(browser == "Chrome" || browser == "Firefox" || browser == "Safari" || browser == "Opera"){
-    alert("Okay we support these browsers too");
-}
+// else if(browser == "Chrome" || browser == "Firefox" || browser == "Safari" || browser == "Opera"){
+//     alert("Okay we support these browsers too");
+// }
 
-else{
-    alert("We hope that this page looks ok!");
+// else{
+//     alert("We hope that this page looks ok!");
+// }
+
+let a = +prompt("Enter a number", "number");
+
+switch(a){
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert(`${2} or ${3}`);
+        break;
+    default:
+        alert("Number not in this system");
 }
