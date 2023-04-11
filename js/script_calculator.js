a2b277556b53aaa50879e26f5843432a6a490e24
dominion-30 <abiolaayobamidele@gmail.com>
2023-04-11 10:26:46 +0100

    Add file

---
+++ js/script_calculator.js
@@ -0,0 +1,23 @@
+
+function addition(num1, num2){
+    let result = num1 + num2;
+    return result;
+
+}
+
+document.getElementById("cD").innerHTML = document.getElementById("one").value;
+
+function subtraction(num1, num2){
+    
+}
+
+function multiplication(num1, num2){
+    let result = num1 * num2;
+    return result;
+}
+
+function division(num1, num2){
+    if (num1 || num2 !=0){
+        let result = num1 / num2; 
+    }
+}
